798a08afa0647923c5bb0cac3c871fa4ebdc7483
Jose Chasey Pratama <josepratama080@gmail.com>
2022-07-23 21:28:03 +0700

    upload
    upload database

---
+++ tbpem.sql
@@ -0,0 +1,281 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 23, 2022 at 04:26 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tbpem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `id_dosen` int(11) NOT NULL,
+  `nama` varchar(30) COLLATE utf16_bin NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` text COLLATE utf16_bin NOT NULL,
+  `jk` enum('l','p') COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `dosen`
+--
+
+INSERT INTO `dosen` (`id_dosen`, `nama`, `tgl_lahir`, `alamat`, `jk`) VALUES
+(0, 'Adihhh2', '0000-00-00', 'Jl. Kolonel Masturi', 'p'),
+(1, 'JOSEH', '2022-07-22', 'JL. Dunia Mars', 'l'),
+(21, 'Adihhh3', '0000-00-00', 'Jl. Kolonel Masturi', 'p');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id_jadwal` int(11) NOT NULL,
+  `id_kelas` int(4) NOT NULL,
+  `id_dosen` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `mulai` time NOT NULL,
+  `selesai` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurusan`
+--
+
+CREATE TABLE `jurusan` (
+  `id_jurusan` int(11) NOT NULL,
+  `nama` varchar(30) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `jurusan`
+--
+
+INSERT INTO `jurusan` (`id_jurusan`, `nama`) VALUES
+(1, 'Teknik Informatika');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(4) NOT NULL,
+  `id_wali` int(11) NOT NULL,
+  `jurusan` int(11) NOT NULL,
+  `tahunid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_wali`, `jurusan`, `tahunid`) VALUES
+(1, 1, 1, 20212);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keys`
+--
+
+CREATE TABLE `keys` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT 0,
+  `is_private_key` tinyint(1) NOT NULL DEFAULT 0,
+  `ip_addresses` text DEFAULT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `keys`
+--
+
+INSERT INTO `keys` (`id`, `user_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
+(1, 1, 'croot', 1, 1, 0, NULL, 1123123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `npm` int(10) NOT NULL,
+  `id_kelas` int(4) NOT NULL,
+  `nama` varchar(20) COLLATE utf16_bin NOT NULL,
+  `alamat` text COLLATE utf16_bin NOT NULL,
+  `jk` enum('l','p') COLLATE utf16_bin NOT NULL,
+  `tgl_lahir` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `id_kelas`, `nama`, `alamat`, `jk`, `tgl_lahir`) VALUES
+(1, 1, 'JOSEH2', 'Jl. Kolonel Masturi', 'l', '2022-07-02'),
+(23, 1, 'Adihhh', 'Jl. Kolonel Masturi', 'l', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mk`
+--
+
+CREATE TABLE `mk` (
+  `id_mk` int(11) NOT NULL,
+  `nama` varchar(10) COLLATE utf16_bin NOT NULL,
+  `sks` int(11) NOT NULL,
+  `kkm` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `mk`
+--
+
+INSERT INTO `mk` (`id_mk`, `nama`, `sks`, `kkm`) VALUES
+(1, 'Design', 24, 70);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `id_matkul` int(11) NOT NULL,
+  `npm` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id_dosen`);
+
+--
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `dosen` (`id_dosen`),
+  ADD KEY `kelas` (`id_kelas`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `kelas_wali` (`id_wali`);
+
+--
+-- Indexes for table `keys`
+--
+ALTER TABLE `keys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`),
+  ADD KEY `fk keals` (`id_kelas`);
+
+--
+-- Indexes for table `mk`
+--
+ALTER TABLE `mk`
+  ADD PRIMARY KEY (`id_mk`);
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `nilai_npm` (`npm`),
+  ADD KEY `nilai_mk` (`id_matkul`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keys`
+--
+ALTER TABLE `keys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `dosen` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id_dosen`),
+  ADD CONSTRAINT `kelas` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`);
+
+--
+-- Constraints for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `kelas_wali` FOREIGN KEY (`id_wali`) REFERENCES `dosen` (`id_dosen`);
+
+--
+-- Constraints for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `fk keals` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`);
+
+--
+-- Constraints for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_mk` FOREIGN KEY (`id_matkul`) REFERENCES `mk` (`id_mk`),
+  ADD CONSTRAINT `nilai_npm` FOREIGN KEY (`npm`) REFERENCES `mahasiswa` (`npm`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
